b39dc9f0174a95e70be6444fd31d59cce540a502
Lessllie Real <rlessllie@yahoo.com>
2020-01-27 07:52:08 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -23,20 +23,20 @@ def third_challenge
    }, 
    :capulet => {
        :patriarch => {
-            :name => "Lord Capulet"
-            :age => "50"
+            name : "Lord Capulet"
+            age : "50"
         #your key/value pairs here
         },
         :matriarch => {
-             :name => "Lady Capulet"
-             :age => "51"
+             name : "Lady Capulet"
+             age : "51"
              
         #your key/value pairs here
         },
         :heroine => {
-              :name => "Juliet"
-              :age => "15"
-              :status => "alive"
+              name : "Juliet"
+              age : "15"
+              status : "alive"
         #your key/value pairs here
         },
         :heroine_friends => []
